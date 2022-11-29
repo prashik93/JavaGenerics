a0a522178f410c591e79a4a9b7c215be3dce27c2
Prashik Kamble <kambleprashik7349@gmail.com>
2022-11-29 15:29:21 +0530

    ADDED : Code to check max float number, take max float number at 1st position.

--- src/com/java_generics/MaximumVariable.java
+++ src/com/java_generics/MaximumVariable.java
@@ -8,17 +8,34 @@ public class MaximumVariable {
         Integer b = 60;
         Integer c = 70;
 
-        Integer maximumInteger = checkMaxAmongThreeNumber(a, b, c);
+        Float p = 7.4f;
+        Float q = 5.2f;
+        Float r = 3.8f;
+
+        Integer maximumInteger = checkMaxAmongThreeIntegerNumber(a, b, c);
+        Float maximumFloat = checkMaxAmongThreeFloatNumber(p, q, r);
         System.out.println("Maximum among 3 Integers Numbers (" + a + ", " + b + ", " + c + ") " + "is : " + maximumInteger);
+        System.out.println("Maximum among 3 Float Numbers (" + p + ", " + q + ", " + r + ") " + "is : " + maximumFloat);
     }
 
-    public static Integer checkMaxAmongThreeNumber(Integer a, Integer b, Integer c) {
+    public static Integer checkMaxAmongThreeIntegerNumber(Integer a, Integer b, Integer c) {
         Integer maximum = a;
         if(b.compareTo(maximum) > 0) {
             maximum = b;
         }
         if(c.compareTo(maximum) > 0) {
             maximum = c;
+        }
+        return maximum;
+    }
+
+    public static Float checkMaxAmongThreeFloatNumber(Float p, Float q, Float r) {
+        Float maximum = p;
+        if(q.compareTo(maximum) > 0) {
+            maximum = q;
+        }
+        if(r.compareTo(maximum) > 0) {
+            maximum = r;
         }
         return maximum;
     }
