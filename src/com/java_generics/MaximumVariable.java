7a9325289450c938c7cf1e154436c4931c7a4eee
Prashik Kamble <kambleprashik7349@gmail.com>
2022-11-29 16:49:45 +0530

    REFACTORED : All the 3 to one generic method and find the maximum

--- src/com/java_generics/MaximumVariable.java
+++ src/com/java_generics/MaximumVariable.java
@@ -16,44 +16,19 @@ public class MaximumVariable {
         String y = "Banana";
         String z = "Peach";
 
-        Integer maximumInteger = checkMaxAmongThreeIntegerNumber(a, b, c);
-        Float maximumFloat = checkMaxAmongThreeFloatNumber(p, q, r);
-        String maximumString = checkMaxAmongThreeStrings(x, y, z);
-        System.out.println("Maximum among 3 Integers Numbers (" + a + ", " + b + ", " + c + ") " + "is : " + maximumInteger);
-        System.out.println("Maximum among 3 Float Numbers (" + p + ", " + q + ", " + r + ") " + "is : " + maximumFloat);
-        System.out.println("Maximum among 3 Strings (" + x + ", " + y + ", " + z + ") " + "is : " + maximumString);
+        checkAmongThreeElements(a, b, c);
+        checkAmongThreeElements(p, q, r);
+        checkAmongThreeElements(x, y, z);
     }
 
-    public static Integer checkMaxAmongThreeIntegerNumber(Integer a, Integer b, Integer c) {
-        Integer maximum = a;
+    public static <T extends Comparable> void checkAmongThreeElements(T a, T b, T c) {
+        T maximum = a;
         if(b.compareTo(maximum) > 0) {
             maximum = b;
         }
         if(c.compareTo(maximum) > 0) {
             maximum = c;
         }
-        return maximum;
-    }
-
-    public static Float checkMaxAmongThreeFloatNumber(Float p, Float q, Float r) {
-        Float maximum = p;
-        if(q.compareTo(maximum) > 0) {
-            maximum = q;
-        }
-        if(r.compareTo(maximum) > 0) {
-            maximum = r;
-        }
-        return maximum;
-    }
-
-    public static String checkMaxAmongThreeStrings(String x, String y, String z) {
-        String maximum = x;
-        if(y.compareTo(maximum) > 0) {
-            maximum = y;
-        }
-        if(z.compareTo(maximum) > 0) {
-            maximum = z;
-        }
-        return maximum;
+        System.out.printf("\nMaximum Among Three Elements i.e (%s, %s, %s) is %s. ",a, b, c, maximum);
     }
 }
